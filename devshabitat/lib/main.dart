af21bc383eb7232515fce0314412692cd622157b
“P3rs0n4l.m3. <tuncsoftwarehere@gmail.com>
2025-07-09 16:08:52 +0300

    did
    old project file deletion

--- devshabitat/lib/main.dart
+++ devshabitat/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:devshabitat/app/firebase/firebase_options.dart';
+import 'package:devshabitat/firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:firebase_core/firebase_core.dart';
